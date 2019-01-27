c58ef5c0c3a87043d052ebba9a19c11bd81b1abe
yang <47072707+ymy55555@users.noreply.github.com>
2019-01-27 17:21:13 +0800

    Create server.c

---
+++ server.c
@@ -0,0 +1,39 @@
+#include "server_common.h"
+extern int gsys_state;
+extern pthread_t gtid[3];
+int main(int argc,char *argv[])
+{
+	signal(SIGINT, common_handle_sig);
+    signal(SIGTERM, common_handle_sig);
+	if(2 != argc)
+	{
+		MY_PRINT("The number of parameters is incorrect.\n");
+		return NORMAL_EXIT;
+	}
+    if(SUCCESS != soctet_init(argv[1]))
+	{
+		MY_PRINTF("soctet init is failed.\n");
+		return NORMAL_EXIT;
+	}
+		
+	if(SUCCESS != create_mian_thread())
+	{
+	    MY_PRINTF("main thread create init is failed.\n");
+		return NORMAL_EXIT;
+	}
+	return NORMAL_EXIT;
+}
+
+
+
+
+/************待实现***********
+1.对所有线程状态给给与检测和控制机制
+2.实现高并发服务器
+3.内核链表标识线程，并设置用户名，并对子线程中的读写线程监控，
+4.互斥锁和读写锁
+5.增加断电重传机制，可以借助临时文件缓存单次读写操作实现
+7.增加动态数据源传输协议，协议格式和长度可以通过文本或者其他指定，且可以引用现有协议。
+8.对整个运行追加全局控制
+9.对每个客户端数据追加标识，且每个客户端单独终端
+*****************************/
