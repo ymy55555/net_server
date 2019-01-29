9d6f854963742fcdaa8696609fed24be893322eb
yang <47072707+ymy55555@users.noreply.github.com>
2019-01-29 09:48:42 +0800

    Update ktest.c

--- ktest.c
+++ ktest.c
@@ -1,17 +1,21 @@
+#if 0
 #include "klist.h"
 
 int main()
 {
     pk_list pk;
 	cli_data cdata = {0};
-	void *client_data;
+	void **client_data;
 	pcli_data pdata;
-	//client_data = (void *)malloc(sizeof(cli_data));
+	*client_data = (void **)malloc(sizeof(cli_data));
 	cdata.client_port = 5001;
     init_klist(&pk);
 	insert_klist(pk, cdata);
 	getdata_klist(pk, 5001, client_data);
-	pdata = (pcli_data)client_data;
+	//指针在下层，数据在上层
+	pdata = (pcli_data)*client_data;
 	printf("-------------port:%d\n", pdata->client_port);
     return 0;
 }
+#endif
+
