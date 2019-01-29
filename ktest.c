b14cfe9c91b7d5d17a536241b0411bb91806ea9e
yang <47072707+ymy55555@users.noreply.github.com>
2019-01-29 17:02:00 +0800

    Update ktest.c

--- ktest.c
+++ ktest.c
@@ -1,18 +1,22 @@
 #if 1
 #include "klist.h"
-
 int main()
 {
     pk_list pk;
 	cli_data cdata = {0};
-	pcli_data *pdata;
-	pdata = (pcli_data *)malloc(sizeof(cli_data));
+	void **client_data;
+	pcli_data pdata;
+	client_data = (void **)malloc(sizeof(cli_data));
 	cdata.client_port = 5001;
+	cdata.client_state = 5;
+	memcpy(cdata.ip_info, (char *)("911.114.120.119"), IP_LENGTH);
     init_klist(&pk);
 	insert_klist(pk, cdata);
-	getdata_klist(pk, 5001, pdata);
+	int port = 5001;
+	getdata_klist(pk, (void *)port, GET_CLIENT_IP_INFO, client_data);
 	//指针在下层，数据在上层
-	printf("-------------port:%d\n", (*pdata)->client_port);
+	pdata = (pcli_data)*client_data;
+	printf("-------------port:%d___ip:%s\n", pdata->client_port, pdata->ip_info);
     return 0;
 }
 #endif
