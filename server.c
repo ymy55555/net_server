1d19293365b8fc4618178b773249ebd6cde8de85
yang <47072707+ymy55555@users.noreply.github.com>
2019-01-30 14:09:24 +0800

    Update server.c

--- server.c
+++ server.c
@@ -1,8 +1,9 @@
 #include "server_common.h"
 #include "common.h"
+#include "klist.h"
+int g_sys_state = 1;
+pk_list g_pklist;
 
-extern int gsys_state;
-extern pthread_t gtid[3];
 int main(int argc,char *argv[])
 {
 	signal(SIGINT, common_handle_sig);
@@ -12,6 +13,11 @@ int main(int argc,char *argv[])
 		MY_PRINTF("The number of parameters is incorrect.\n");
 		return NORMAL_EXIT;
 	}
+	if (SUCCESS != init_klist(&g_pklist))
+    {
+        MY_PRINTF("Init_klist failed.\n");
+        return NORMAL_EXIT;
+    }
     if(SUCCESS != socKet_init(argv[1]))
 	{
 		MY_PRINTF("soctet init is failed.\n");
@@ -26,9 +32,6 @@ int main(int argc,char *argv[])
 	return NORMAL_EXIT;
 }
 
-
-
-
 /************待实现***********
 1.对所有线程状态给给与检测和控制机制
 2.实现高并发服务器
@@ -38,4 +41,5 @@ int main(int argc,char *argv[])
 7.增加动态数据源传输协议，协议格式和长度可以通过文本或者其他指定，且可以引用现有协议。
 8.对整个运行追加全局控制
 9.对每个客户端数据追加标识，且每个客户端单独终端
-*****************************/
+10.数据规范化，比如对齐之类
+****************************/
